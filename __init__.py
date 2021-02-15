d28e2b9fd4474c22b8b237a703611f8d68fea1de
Boris <478635054@qq.com>
2021-02-15 21:48:23 +0800

    Optimize project architecture

---
+++ __init__.py
@@ -0,0 +1,114 @@
+import pygame
+import random
+import sys
+from pygame.locals import *
+import variables
+class Point():
+    row = 0
+    column = 0
+
+    def __init__(self,row,column):
+        self.row = row
+        self.column = column
+
+    def copy(self):
+        return Point(row = self.row,column = self.column)
+
+pygame.init()
+
+head = Point(row = random.randint(3,variables.ROW - 3),column = random.randint(3,variables.COLUMN - 3))
+
+snake = [
+    Point(row = head.row,column = head.column + 1),
+    Point(row = head.row,column = head.column + 2),
+]
+
+def getfood():
+    while 1:
+        position = Point(row = random.randint(0,variables.ROW - 1),column = random.randint(0,variables.COLUMN - 1))
+        overlap = False
+        if position.column == head.column and position.row == head.row:
+            overlap = True
+        for body in snake:
+            if position.column == body.column and position.row == body.row:
+                overlap = True
+                break
+        if overlap == False:
+            break
+    return position
+
+def draw(Point,color):
+    left = Point.column * variables.width / variables.COLUMN
+    top = Point.row * variables.height / variables.ROW
+    pygame.draw.rect(variables.screen,color,(left,top,variables.width / variables.COLUMN,variables.height / variables.ROW))
+
+food = getfood()
+
+# insert the image and adapt it to the screen
+backgroundimage = pygame.image.load('background.png')
+newingimage = pygame.transform.scale(backgroundimage,(variables.width,variables.height))
+variables.screen.blit(newingimage,(0,0))
+pygame.display.set_caption('Greedy Snake')
+
+running = True
+clock = pygame.time.Clock()
+while running:
+    clock.tick(20)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # monitor the keyboard
+        elif event.type == KEYDOWN:
+            if event.key == K_UP:
+                if variables.direct == 'right' or variables.direct == 'left':
+                    variables.direct = 'up'
+            if event.key == K_DOWN:
+                if variables.direct == 'right' or variables.direct == 'left':
+                    variables.direct = 'down'
+            if event.key == K_RIGHT:
+                if variables.direct == 'up' or variables.direct == 'down':
+                    variables.direct = 'right'
+            if event.key == K_LEFT:
+                if variables.direct == 'up' or variables.direct =='down':
+                    variables.direct ='left'
+
+    #offer the food
+    eat = (head.row == food.row and head.column == food.column)
+    if eat:
+        food = Point(row = random.randint(0,variables.ROW - 1),column = random.randint(0,variables.COLUMN -1))
+    snake.insert(0,head.copy())
+    if not eat:
+        snake.pop()
+
+    # change the direction
+    if variables.direct == 'up':
+        head.row -= 1
+    if variables.direct == 'down':
+        head.row += 1
+    if variables.direct == 'right':
+        head.column += 1
+    if variables.direct == 'left':
+        head.column -= 1
+
+    # identify the death of the snake
+    death = False
+    if head.row < 0 or head.column < 0 or head.row >= variables.ROW or head.column >= variables.COLUMN:
+        death = True
+    for body in snake:
+        if head.row == body.row and head.column == body.column:
+            death = True
+            break
+    if death:
+        print('Game over')
+        running = False
+
+    # draw objects
+    backgroundimage = pygame.image.load('background.png')
+    newingimage = pygame.transform.scale(backgroundimage,(variables.width,variables.height))
+    variables.screen.blit(newingimage,(0,0))
+    draw(head, variables.head_color)
+    draw(food, variables.food_color)
+    for body in snake:
+        draw(body, variables.snake_color)
+    pygame.display.flip()
---
+++ variables.py
@@ -0,0 +1,31 @@
+import pygame
+
+width = 800
+height = 600
+ROW = 30
+COLUMN = 40
+direct = 'left'
+head_color = (255,106,106)
+snake_color = (160,32,240)
+food_color = (255,165,0)
+screen = pygame.display.set_mode((width,height))
+
+def variable():
+    global width
+    global height
+    global ROW
+    global COLUMN
+    global direct
+    global head_color
+    global snake_color
+    global food_color
+    global screen
+    width = 800
+    height = 600
+    ROW = 30
+    COLUMN = 40
+    direct = 'left'
+    ead_color = (255,106,106)
+    snake_color = (160,32,240)
+    food_color = (255,165,0)
+    screen = pygame.display.set_mode((width,height))
